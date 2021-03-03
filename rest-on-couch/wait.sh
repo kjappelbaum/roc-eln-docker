719b2f779268faed4ed60437bceeebd7fc78e9e3
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-03-03 13:36:50 +0100

    chore: reverting changes to debug

--- rest-on-couch/wait.sh
+++ rest-on-couch/wait.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while [ $(curl --write-out %{http_code} --silent --output /dev/null http://${COUCHDB_HOST}:5984/_users) == "000" ]; do
+while [ $(curl --write-out %{http_code} --silent --output /dev/null http://couchdb:5984/_users) == "000" ]; do
     echo "CouchDB is starting up..."
     sleep 3
 done
